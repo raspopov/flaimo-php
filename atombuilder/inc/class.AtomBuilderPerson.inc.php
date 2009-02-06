c453e5586ff0c245c558e7719663fe7cc458c3f3
Michael Wimmer <flaimo@gmail.com>
2009-02-06 23:41:25 +0000

    replaced eregi function to work with php 5.3

--- atombuilder/inc/class.AtomBuilderPerson.inc.php
+++ atombuilder/inc/class.AtomBuilderPerson.inc.php
@@ -22,21 +22,8 @@ class AtomBuilderPerson extends AtomBuilderBase {
 	} // end constructor
 
 	protected function isValidMail($mail = '') {
-		$regex =
-		  '^'.
-		  '[_a-z0-9-]+'.        /* One or more underscore, alphanumeric,
-		                           or hyphen charactures. */
-		  '(\.[_a-z0-9-]+)*'.  /* Followed by zero or more sets consisting
-		                           of a period and one or more underscore,
-		                           alphanumeric, or hyphen charactures. */
-		  '@'.                  /* Followed by an "at" characture. */
-		  '[a-z0-9-]+'.        /* Followed by one or more alphanumeric
-		                           or hyphen charactures. */
-		  '(\.[a-z0-9-]{2,})+'. /* Followed by one or more sets consisting
-		                           of a period and two or more alphanumeric
-		                           or hyphen charactures. */
-		  '$';
-		  return eregi($regex, $mail);
+		$regex = '/^([a-z0-9])(([-a-z0-9._])*([a-z0-9]))*\@([a-z0-9])' . '(([a-z0-9-])*([a-z0-9]))+' . '(\.([a-z0-9])([-a-z0-9_-])?([a-z0-9])+)+$/i';
+		return preg_match ($regex, $mail);
 	} // end function
 
 	public function setName($string = '') {
